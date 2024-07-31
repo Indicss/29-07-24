5118720bbc9e4954a8847739aa172f6aedb806ec
Jalba Oleg <jalbaoleg00@gmail.com>
2024-07-31 14:59:26 +0300

    f

---
+++ 29 07 24/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public interface IWorker
+{
+    void Work();
+}
+
+public interface IHumanWorker : IWorker
+{
+    void Eat();
+}
+
+public class HumanWorker : IHumanWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Human working...");
+    }
+
+    public void Eat()
+    {
+        Console.WriteLine("Human eating...");
+    }
+}
+
+public class RobotWorker : IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Robot working...");
+    }
+}
+
+public class WorkManager
+{
+    private readonly IWorker _worker;
+
+    public WorkManager(IWorker worker)
+    {
+        _worker = worker;
+    }
+
+    public void ManageWork()
+    {
+        _worker.Work();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IWorker humanWorker = new HumanWorker();
+        IWorker robotWorker = new RobotWorker();
+
+        WorkManager humanManager = new WorkManager(humanWorker);
+        WorkManager robotManager = new WorkManager(robotWorker);
+
+        humanManager.ManageWork();
+        robotManager.ManageWork();
+    }
+}
